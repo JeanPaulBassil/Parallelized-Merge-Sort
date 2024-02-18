32e6b5456438333d958ca8f122130701113dbdc9
Jean-Paul <jean.paul.bassil@outlook.com>
2024-02-18 14:35:13 +0200

    add: implemented merge sort sequencially

---
+++ parallelizedMergeSort.c
@@ -0,0 +1,17 @@
+
+
+int main() {
+
+
+    return 0;
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
